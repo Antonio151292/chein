dc433f0db1d014da53315547bb8956387fe38e0c
Antonio <antonio151292@hotmail.com>
2022-05-29 11:04:17 -0500

    impresion de login y menu de usuario

--- header.php
+++ header.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <?php
 include 'views/php/conexion.php';
-session_start();
-$ID_Usuario = ($_SESSION['ID_Usuario']);
-print_r($ID_Usuario);
+
+
 ?>
 <html>
 
@@ -60,43 +59,11 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 <?php
 include 'views/php/conexion.php';
-
 /***************************************************************************************************** */
 /* Se necesita validar la sesion activa para que cambie entre el model de iniciar sesio y el de cerrar */
 /***************************************************************************************************** */
-if (isset($_SESSION['ID_Usuario'])) {
-    echo "
-    <div class='modal fade' id='myModal88' tabindex='-1' role='dialog' aria-labelledby='myModal88' aria-hidden='true'>
-        <div class='modal-dialog modal-lg'>
-            <div class='modal-content'>
-                <div class='modal-header'>
-                    <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>
-                    &times;</button>
-                    <h4 class='modal-title' id='myModalLabel'>
-                    ¡Bienvenido!</h4>
-                </div>
-                <div class='modal-body modal-body-sub'>
-                    <div class='row'>
-                        <div class='col-md-8 modal_body_left modal_body_left1' >
-                            <div class='sap_tabs'>
-                                <ul>
-                                    <li>Perfil</li>
-                                    <li>Compras</li>
-                                    <li>Facturación</li>
-                                </ul>   
-                                <br>
-                                <div id='horizontalTab' style='display: block; width: 100%; margin: 0px;'>
-                                    <button onclick='location.href='views/php/session_close.php'' class='nav_link cerrar' title='Cerrar sesión'> <i class='fas fa-sign-out-alt' style='font-size: 1.5rem'></i> <span class='nav_name'>Cerrar sesión</span></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    ";
-} else {
+session_start();
+if (!isset($_SESSION['ID_Usuario'])) {
     echo "
     <div class='modal fade' id='myModal88' tabindex='-1' role='dialog' aria-labelledby='myModal88' aria-hidden='true'>
     <div class='modal-dialog modal-lg'>
@@ -107,6 +74,56 @@ if (isset($_SESSION['ID_Usuario'])) {
                 <h4 class='modal-title' id='myModalLabel'>
                     ¡Inicie sesión ahora!</h4>
             </div>
+            <div class='modal-body modal-body-sub'>
+                <div class='row'>
+                    <div class='col-md-8 modal_body_left modal_body_left1' style='border-right: 1px dotted #C2C2C2;padding-right:3em;'>
+                        <div class='sap_tabs'>
+                            <div id='horizontalTab' style='display: block; width: 100%; margin: 0px;'>
+                                <ul>
+                                    <li class='resp-tab-item' aria-controls='tab_item-0'><span>Iniciar sesión</span></li>
+                                    <li class='resp-tab-item' aria-controls='tab_item-1'><span>Regístrate</span></li>
+                                </ul>
+                                <div class='tab-1 resp-tab-content' aria-labelledby='tab_item-0'>
+                                    <div class='facts'>
+                                        <div class='register'>
+                                            <form action='views/php/iniciar-sesion.php' method='post'>
+                                                <input name='Email' placeholder='Correo' type='text' required=''>
+                                                <input name='Password' placeholder='Contraseña' type='password' required=''>
+                                                <div class='sign-up'>
+                                                    <input type='submit' value='Iniciar sesión' />
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class='tab-2 resp-tab-content' aria-labelledby='tab_item-1'>
+                                    <div class='facts'>
+                                        <div class='register'>
+                                            <form action='views/php/registrar-usuario.php' method='post'>
+                                                <input placeholder='Correo' name='Email' type='email' required=''>
+                                                <input placeholder='Contraseña' name='Password' type='password' required=''>
+                                                <div class='sign-up'>
+                                                    <input type='submit' value='Crear cuenta' />
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <script src='js/easyResponsiveTabs.js' type='text/javascript'></script>
+                        <script type='text/javascript'>
+                            $(document).ready(function() {
+                                $('#horizontalTab').easyResponsiveTabs({
+                                    type: 'default', //Types: default, vertical, accordion           
+                                    width: 'auto', //auto or any width like 600px
+                                    fit: true // 100% fit in a container
+                                });
+                            });
+                        </script>
+                        <div id='OR' class='hidden-xs'>
+                            O</div>
+                    </div>
                     <div class='col-md-4 modal_body_right modal_body_right1'>
                         <div class='row text-center sign-with'>
                             <div class='col-md-12'>
@@ -137,9 +154,41 @@ if (isset($_SESSION['ID_Usuario'])) {
         </div>
 </div>
     ";
+} else {
+    echo "
+    <div class='modal fade' id='myModal88' tabindex='-1' role='dialog' aria-labelledby='myModal88' aria-hidden='true'>
+    <div class='modal-dialog modal-lg'>
+    <div class='modal-content'>
+            <div class='modal-header'>
+                <button type='button' class='close' data-dismiss='modal' aria-hidden='true'>
+                    &times;</button>
+                <h4 class='modal-title' id='myModalLabel'>
+                    ¡Inicie sesión ahora!</h4>
+            </div>
+            <div class='modal-body modal-body-sub'>
+                <div class='row'>
+                    <div class='col-md-8 modal_body_left modal_body_left1' style='border-right: 1px dotted #C2C2C2;padding-right:3em;'>
+                    <div class='sap_tabs'>
+                    <ul>
+                        <li>Perfil</li>
+                        <li>Compras</li>
+                        <li>Facturación</li>
+                    </ul>   
+                    <br>
+                    <div id='horizontalTab' style='display: block; width: 100%; margin: 0px;'>
+                        <a href='views/php/session_close.php'' class='nav_link cerrar' title='Cerrar sesión'> <i class='fas fa-sign-out-alt' style='font-size: 1.5rem'></i> <span class='nav_name'>Cerrar sesión</span></a>
+                    </div>
+                </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        </div>
+</div>
+    ";
     
 };
-
 
 ?>
 
